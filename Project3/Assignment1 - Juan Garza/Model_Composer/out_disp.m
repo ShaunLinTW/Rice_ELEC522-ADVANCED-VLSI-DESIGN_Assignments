aa5c0327c2c72e6d4c979aa06d56b6b37281fe80
Shaun Lin <yoyomy7198come@gmail.com>
2022-12-24 14:58:19 -0600

    Delete Project3/Assignment1 - Juan Garza directory

--- Project3/Assignment1 - Juan Garza/Model_Composer/out_disp.m
+++
@@ -1,9 +0,0 @@
-out_c_1var = out.c_1var.DATA(1:30).';
-out_c_2var = out.c_2var.DATA(1:30).';
-out_c_3var = out.c_3var.DATA(1:30).';
-out_c_4var = out.c_4var.DATA(1:30).';
-
-disp(out_c_1var)
-disp(out_c_2var)
-disp(out_c_3var)
-disp(out_c_4var)
